520d9d1dcea812888f19a4334373c36f63706d67
Gaurav <flexi.aries@gmail.com>
2013-06-04 12:18:07 +0530

    Added AddedMember (UIView) for newly added members, set the content size of AddMember Scrollview

--- MyTracker/MyTracker/MemberView.m
+++ MyTracker/MyTracker/MemberView.m
@@ -12,14 +12,18 @@
 #define kTopPadding 4
 #define kAddMemberImageViewWidth 100
 #define kAddMemberImageViewHeight 50
+#define kHorizontalScrollPadding 10
 
 @implementation MemberView
 
+// the variable 'memberIndex' is incremented everytime a new member is added, this serves two purpose - 1) Tells the total number of added members in this class and 2) helps in setting CGRect/frame for adding new members
 int memberIndex = 0;
 
 #pragma mark View lifecycle
 
 -(void)awakeFromNib {
+    
+    // addMemberImageView is an image view, touching which allows user to add members to the captured location
     addMemberImageView = [[UIImageView alloc] initWithFrame:CGRectMake(kLeftPadding, kTopPadding, kAddMemberImageViewWidth, kAddMemberImageViewHeight)];
     addMemberImageView.image = [UIImage imageNamed:@"plus.png"];
     [self addSubview:addMemberImageView];
@@ -32,42 +36,51 @@ int memberIndex = 0;
 #pragma mark Single Tap Gesture
 
 -(void)handleTouch:(UITapGestureRecognizer *)gesture {
+    NSLog(@"Single tap");
     [self.memberViewDelegate addMemberTouched];
-}
-
-#pragma mark Long Press Gesture
-
--(void)handleLongPress:(UILongPressGestureRecognizer *)gesture {
-    NSLog(@"----------------------");
 }
 
 #pragma mark Displaying Added members
 
 -(void)displayAddedMemberWithName:(NSString *)name andRelation:(NSString *)relation  {
-    // setting frame for newly added member, and adding it
-    CGRect nameLabelFrame = CGRectMake((kLeftPadding * (memberIndex + 1)) + (memberIndex * kAddMemberImageViewWidth),
-                                       kTopPadding,
-                                       kAddMemberImageViewWidth,
-                                       kAddMemberImageViewHeight);
+   
+    // adding a new UIView for an added member
+    CGRect addedMemberFrame = CGRectMake((kLeftPadding * (memberIndex + 1)) + (memberIndex * kAddMemberImageViewWidth),
+                                                  kTopPadding,
+                                                  kAddMemberImageViewWidth,
+                                                  kAddMemberImageViewHeight);
+    UIView *addedMember = [[UIView alloc] initWithFrame:addedMemberFrame];
+    addedMember.backgroundColor = [UIColor blueColor];
+    [self addSubview:addedMember];
     
-    UILabel *nameLabel = [[UILabel alloc] initWithFrame:nameLabelFrame];
+    // setting frame for added member, and adding it
+    UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, kAddMemberImageViewWidth, kAddMemberImageViewHeight)];
     nameLabel.text = name;
     nameLabel.textAlignment = UITextAlignmentCenter;
-    [self addSubview:nameLabel];
-    
-    UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
-    [nameLabel addGestureRecognizer:longPressGesture];
-    [nameLabel setUserInteractionEnabled:YES];
+    [addedMember addSubview:nameLabel];
     
     memberIndex ++;
 
+    [self manageContentSize];
+    [self rePositionAddMemberImageView];
+}
+
+#pragma mark Content Size for ScrollView
+
+-(void)manageContentSize {
+    [self setContentSize:CGSizeMake((memberIndex * (kLeftPadding + kAddMemberImageViewWidth)) + kAddMemberImageViewWidth + kHorizontalScrollPadding, 0)];
+}
+
+#pragma mark Re-position of AddMember Image View
+
+-(void)rePositionAddMemberImageView {
     // shifting AddMemberImageView placeholder to its next position
-    CGRect newFrameForAddMember = CGRectMake((kLeftPadding * (memberIndex + 1)) + (memberIndex * kAddMemberImageViewWidth),
-                                 kTopPadding,
-                                 kAddMemberImageViewWidth,
-                                 kAddMemberImageViewHeight);
+    CGRect frameForAddMemberPlaceholder = CGRectMake((kLeftPadding * (memberIndex + 1)) + (memberIndex * kAddMemberImageViewWidth),
+                                                     kTopPadding,
+                                                     kAddMemberImageViewWidth,
+                                                     kAddMemberImageViewHeight);
     
-    [addMemberImageView setFrame:newFrameForAddMember];    
+    [addMemberImageView setFrame:frameForAddMemberPlaceholder];
 }
 
 @end
